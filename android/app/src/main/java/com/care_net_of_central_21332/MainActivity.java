2106992d5cd42b2280f1312fb06ddad6e2c8d70e
Crowdbotics <team@crowdbotics.com>
2020-10-10 15:41:43 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/care_net_of_central_21332/MainActivity.java
@@ -0,0 +1,15 @@
+package com.care_net_of_central_21332;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "care_net_of_central_21332";
+    }
+}
---
+++ backend/care_net_of_central_21332/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for care_net_of_central_21332 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'care_net_of_central_21332.settings')
+
+application = get_wsgi_application()
